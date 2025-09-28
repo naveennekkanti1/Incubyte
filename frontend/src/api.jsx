44d9e6f8218ffad2bd10f397d518deb1db46419f
Durga Naveen Nekkanti <111896305+naveennekkanti1@users.noreply.github.com>
2025-09-28 12:36:01 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://incubyte-9rtg.onrender.com/api",
 });
 
 // Attach token if present
